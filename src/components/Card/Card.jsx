cc4706412aef34f9b65101e4a798367cc23006a7
Caio Azevedo <136472218+justCaioDev@users.noreply.github.com>
2024-09-13 14:21:22 -0300

    fix: fix the images

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,19 @@
 import './Card.css'
+import CardImage from '../../assets/illustration-article.svg'
+import AuthorPhoto from '../../assets/image-avatar.webp'
 import PropTypes from 'prop-types';
 
 function Card({ title, content, status, date }) {
   return (
     <div className='box'>
         <article className='card-container'>
-            <img className='card-image' src="/src/assets/illustration-article.svg" alt="card image" />
+            <img className='card-image' src={CardImage} alt="card image" />
             <p className='status'>{status}</p>
             <p className='publish-date'>Published {date}</p>
             <h1 className='card-title'>{title}</h1>
             <p className='card-content'>{content}</p>
             <div className="author">
-                <img className='author-photo' src="/src/assets/image-avatar.webp" alt="a image of author" />
+                <img className='author-photo' src={AuthorPhoto} alt="a image of author" />
                 <p className='author-name'>Greg Hooper</p>
             </div>
         </article>
